94ebeb67733b92943e72470dceade3938d5d042c
AdamAjmi <adam.ajmi@gmail.com>
2015-05-21 23:39:51 +0200

    små endringer i lab 3
    småplukk, endring av filnavn osv.

--- Lab 3/udpclient.py
+++ Lab 3/udpclient.py
@@ -1,12 +1,23 @@
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 from socket import *
+#Tillater oss å lage sockets i python.
 
-s_address = 'localhost'
+s_address = '127.0.0.1'
+#Navn på server. 127.0.0.1 = localhost.
 s_port = 50007
 c_socket = socket(AF_INET, SOCK_DGRAM)
+#AF_INET = IPv4 | SOCK_DGRAM = UDP.
 
 while 1:  
+#Klienten kjører på udefinert tid fram all kode i 
+#while-statement har blitt kjørt.    
+    uni_lower = raw_input('Lower-case unicode letter to be capitalized: ')
+    c_socket.sendto(uni_lower, (s_address, s_port))
+    #Sender variabel til server.
+    uni_upper, s_address = c_socket.recvfrom(4096)
+    #socket for å motta fange opp meldinger fra server.
+    print uni_upper
     
     roman_num = raw_input('Roman number to be converted to an arabic number:')
     c_socket.sendto(roman_num, (s_address, s_port))
@@ -16,6 +27,13 @@ while 1:
     arab_num = raw_input('Arabic number to be converted to roman:')
     c_socket.sendto(arab_num, (s_address, s_port))
     fromArab, s_address = c_socket.recvfrom(4096)
-    print fromArab
+    print fromArab   
+    
+    num1 = raw_input('First roman number: ')
+    num2 = raw_input('Second roman number: ')
+    c_socket.sendto(num1, (s_address, s_port))
+    c_socket.sendto(num2, (s_address, s_port))
+    added_num, s_address = c_socket.recvfrom(4096)
+    print added_num
     
 c_socket.close()
--- Lab 3/udpserver.py
+++ Lab 3/udpserver.py
@@ -1,21 +1,43 @@
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 from socket import *
-import lab2
+#Tillater oss å lage sockets i python.
+import romanmodule
+#Modul fra lab 2 som gjør at vi kan utføre operasjoner
+#med romerske numeraler.
 
 s_port = 50007
 s_socket = socket(AF_INET, SOCK_DGRAM)
+#AF_INET = IPv4 | SOCK_DGRAM = UDP
 s_socket.bind(('', s_port))
-print 'The server has been set up and is ready to recieve data.'
+#Knytter s_port til sokkelen slik at all informasjon sendt til porten
+#fanges av server.
+print 'The server has been initialized and is listening.'
+
 
 while 1:
+#Serveren looper til den stanses manuellt.
+    
+    uni_lower, c_address = s_socket.recvfrom(4096)
+    #Når en melding mottas i socket, legges verdien inn i den første variabelen.
+    #Den andre variabelen lagrer sendes addresse slik at en melding
+    #kan returneres senere.
+    uni_upper = uni_lower.decode('utf-8').upper()
+    s_socket.sendto(uni_upper, c_address)
+    #Sender svar tilbake til klientens addresse.
     
     roman_num, c_address = s_socket.recvfrom(4096)
-    print 'Roman numeral to be converted: ' + roman_num
-    fromRoman = lab2.roman_to_int(roman_num)
-    print 'Converted roman numeral to send to client'
+    fromRoman = romanmodule.roman_to_int(roman_num)
+    #Bruker importert metode fra romanmodule.
     s_socket.sendto(str(fromRoman), c_address)
     
     arab_num, c_address = s_socket.recvfrom(4096)
-    fromArab = lab2.int_to_roman(arab_num)
-    s_socket.sendto(fromArab, c_address)
+    arab_num2 = int(arab_num)
+    fromArab = romanmodule.int_to_roman(arab_num2)
+    s_socket.sendto(str(fromArab), c_address)
+    
+    num1, c_address = s_socket.recvfrom(4096)
+    num2, c_address = s_socket.recvfrom(4096)
+    added_num = romanmodule.roman_add(num1, num2)
+    s_socket.sendto(str(added_num), c_address)
+    
