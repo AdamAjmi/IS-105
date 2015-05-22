c75fc58cdc94253c30264c9a7eb2e9394791231f
AdamAjmi <adam.ajmi@gmail.com>
2015-05-22 13:53:37 +0200

    Fungerende Lab 3
    Funksjonaliteten er dårlig men den funker helt greit. Hadde hatt godt av
    mer feilbehandling.

--- Lab 3/udpclient.py
+++ Lab 3/udpclient.py
@@ -1,39 +1,67 @@
-# -*- coding: utf-8 -*-
+# -*- coding: latin-1 -*-
 
 from socket import *
 #Tillater oss å lage sockets i python.
-
-s_address = '127.0.0.1'
-#Navn på server. 127.0.0.1 = localhost.
-s_port = 50007
+s_port = 10044
 c_socket = socket(AF_INET, SOCK_DGRAM)
 #AF_INET = IPv4 | SOCK_DGRAM = UDP.
-
-while 1:  
+c_socket.settimeout(3)
+loop = True
+while loop:  
 #Klienten kjører på udefinert tid fram all kode i 
 #while-statement har blitt kjørt.    
-    uni_lower = raw_input('Lower-case unicode letter to be capitalized: ')
-    c_socket.sendto(uni_lower, (s_address, s_port))
-    #Sender variabel til server.
-    uni_full, s_address = c_socket.recvfrom(4096)
-    #socket for å motta fange opp meldinger fra server.
-    print uni_full
+        
+        print "The server can convert lower-case unicode letters to capitalized unicode letters."
+        s_address = 'localhost'
+        #Navn på server. 127.0.0.1 = localhost.       
+        print 'Type in the lower-case unicode letter you want capitalized:'
+        uni_lower = raw_input('> ')
+        c_socket.sendto(uni_lower, (s_address, s_port))
+        #Sender variabel til server.
+        print 'The letter was sent to the server. Waiting for response...'
+        uni_full, s_address = c_socket.recvfrom(4096)
+        #socket for å kunne motta og fange opp meldinger fra server.
+        print uni_full
     
-    roman_num = raw_input('Roman number to be converted to an arabic number:')
-    c_socket.sendto(roman_num, (s_address, s_port))
-    fromRoman, s_address = c_socket.recvfrom(4096)
-    print fromRoman
-    
-    arab_num = raw_input('Arabic number to be converted to roman:')
-    c_socket.sendto(arab_num, (s_address, s_port))
-    fromArab, s_address = c_socket.recvfrom(4096)
-    print fromArab   
-    
-    num1 = raw_input('First roman number: ')
-    num2 = raw_input('Second roman number: ')
-    c_socket.sendto(num1, (s_address, s_port))
-    c_socket.sendto(num2, (s_address, s_port))
-    added_num, s_address = c_socket.recvfrom(4096)
-    print added_num
-    
-c_socket.close()
+        print "--------------------------------------------------------------------"
+        
+        print "The server can convert roman numbers into arabic numbers."
+        s_address = 'localhost'       
+        print 'Type in the roman number you would like to be converted to arabic:'
+        roman_num = raw_input('> ')
+        c_socket.sendto(roman_num, ((s_address), (s_port)))
+        print "The roman number was sent to the server. Waiting for response..."
+        fromRoman, s_address = c_socket.recvfrom(4096)
+        print fromRoman
+        
+        print "--------------------------------------------------------------------"
+        
+        print "It can also convert arabic numbers into roman numbers."
+        s_address = 'localhost'       
+        print 'Type in the arabic number you would like to be converted to roman:'
+        arab_num = raw_input('> ')
+        c_socket.sendto(arab_num, ((s_address), (s_port)))
+        print "The arabic number was sent to the server. Waiting for response..."
+        fromArab, s_address = c_socket.recvfrom(4096)
+        print fromArab
+        
+        print "--------------------------------------------------------------------"
+
+        print "Lastly the server can add two roman numerals together."
+        s_address = 'localhost'       
+        print 'Type in the first roman number you would like to add: '
+        num1 = raw_input('> ')
+        print 'Type in the second roman number you would like to add: '
+        num2 = raw_input('> ')
+        c_socket.sendto(num1, ((s_address), (s_port)))
+        c_socket.sendto(num2, ((s_address), (s_port)))
+        print 'The numbers were sent to the server. Waiting for response...'
+        added_num, s_address = c_socket.recvfrom(4096)
+        print "" + num1 + " + " + num2 + " = " + added_num + ""
+        
+        print "--------------------------------------------------------------------"
+        
+        print "Succesfully ran all functions. Closing the socket..."
+        loop = False
+        c_socket.close
+ 
--- Lab 3/udpserver.py
+++ Lab 3/udpserver.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- coding: latin-1 -*-
 
 from socket import *
 #Tillater oss å lage sockets i python.
@@ -6,7 +6,7 @@ import romanmodule
 #Modul fra lab 2 som gjør at vi kan utføre operasjoner
 #med romerske numeraler.
 
-s_port = 50007
+s_port = 10044
 s_socket = socket(AF_INET, SOCK_DGRAM)
 #AF_INET = IPv4 | SOCK_DGRAM = UDP
 s_socket.bind(('', s_port))
@@ -22,23 +22,39 @@ while 1:
     #Når en melding mottas i socket, legges verdien inn i den første variabelen.
     #Den andre variabelen lagrer sendes addresse slik at en melding
     #kan returneres senere.
+    print "Lower_case letter '" + uni_lower + "'  recieved from client."
     uni_upper = uni_lower.decode('utf-8').upper()
     uni_full = uni_upper.encode('utf-8')
+    print "Letter was converted to '" + uni_full + "'. Sending back to client:"
     s_socket.sendto(uni_full, c_address)
+    print "Letter was sent to the client."
     #Sender svar tilbake til klientens addresse.
+    print "----------------------------------------------------"
     
     roman_num, c_address = s_socket.recvfrom(4096)
+    print "Roman numeral '" + roman_num + "' recieved from client."
     fromRoman = romanmodule.roman_to_int(roman_num)
+    print "The numeral was converted to arabic numerals."
     #Bruker importert metode fra romanmodule.
     s_socket.sendto(str(fromRoman), c_address)
+    print "The arabic numeral was sent to the client."
+    print "----------------------------------------------------"
     
     arab_num, c_address = s_socket.recvfrom(4096)
+    print "Arabic numeral '" + arab_num + "' recieved from client."
     arab_num2 = int(arab_num)
     fromArab = romanmodule.int_to_roman(arab_num2)
+    print "The numeral was converted to '" + fromArab + "' in roman numerals."
     s_socket.sendto(str(fromArab), c_address)
+    print "The roman numeral was sent to the client."
+    print "----------------------------------------------------"
     
     num1, c_address = s_socket.recvfrom(4096)
+    print "The first roman numeral to be added is '" + num1 + "'"
     num2, c_address = s_socket.recvfrom(4096)
+    print "The second roman numeral to be added is '" + num2 + "'"
     added_num = romanmodule.roman_add(num1, num2)
+    print "The two numerals were added to a total of '" + added_num + "' in roman numerals."
     s_socket.sendto(str(added_num), c_address)
-    
+    print "The combined roman numerals were sent to the client."
+    print "----------------------------------------------------"
